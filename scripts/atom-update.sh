86119d0f846fe4ecf90581f554b06bbc884eef62
Shivansh Rai <shivanshrai84@gmail.com>
2016-08-17 19:48:39 +0530

    Add script for automating atom upgrade

---
+++ scripts/atom-update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Update atom from downloaded deb file
+
+rm -f /tmp/atom.deb
+curl -L https://atom.io/download/deb > /tmp/atom.deb
+dpkg --install /tmp/atom.deb
+
+# Don't need su handling apm commands.
+# apm upgrade --confirm false
+
+# atom
+exit 0
