3c41745e23b696966e2da51db96d955182e88e5b
Shivansh Rai <shivansh@freebsd.org>
2018-10-05 02:51:14 +0530

    vimfiles: more updates in accordance with 963a0c

--- vimfiles/skeleton/skeleton.cpp
+++ vimfiles/skeleton/skeleton.cpp
@@ -1,12 +1,14 @@
 /**
- *      Author:        shivansh
- *      Created:       TIMESTAMP
- *      Last Modified: TIMESTAMP
-**/
+ *      Author:  shivansh
+ *      Created: TIMESTAMP
+ **/
 
 #include <bits/stdc++.h>
 
 using namespace std;
+
+typedef long long ll;
+const int inf = 1e9;
 
 int main() {
     return 0;
